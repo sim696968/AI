b49c8501c5cc92e3038b5eb0e5c92941d5f0eeec
sim696968 <zim957720@gmail.com>
2025-11-10 13:13:59 +0800

    Add files via upload

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           react: ['react', 'react-dom'],
-          vendor: ['react-router-dom', 'react-icons']
+          vendor: ['react-icons']
         }
       }
     },
